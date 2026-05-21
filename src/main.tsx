87a80473b1e1b258aa818f422be6644d4dfa90de
Jorge Altamirano <grisjaa@gmail.com>
2026-05-21 20:03:49 -0300

    feat: add automatic background polling and tab-focus synchronization

--- src/main.tsx
+++ src/main.tsx
@@ -8,8 +8,25 @@ function Root() {
   const syncFromServer = useStore((s) => s.syncFromServer);
 
   useEffect(() => {
+    // Sincronización inicial al cargar
     syncFromServer();
-  }, []);
+
+    // Sincronizar periódicamente cada 30 segundos
+    const interval = setInterval(() => {
+      syncFromServer();
+    }, 30000);
+
+    // Sincronizar automáticamente cuando el usuario regresa a la pestaña (refetch on focus)
+    const handleFocus = () => {
+      syncFromServer();
+    };
+    window.addEventListener('focus', handleFocus);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('focus', handleFocus);
+    };
+  }, [syncFromServer]);
 
   return (
     <StrictMode>
